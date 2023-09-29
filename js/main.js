3047dcf69d35abc35156cecddc12991f31680a35
Oscar Manuel López Mojica <lopezoscarmanolo@gmail.com>
2023-09-28 19:04:12 -0600

    segunda pre-entrega

--- js/main.js
+++ js/main.js
@@ -22,3 +22,43 @@
          document.getElementById("resultado").innerHTML = "El número es más pequeño. Intenta de nuevo.";
      }
 }
+const personas = [];
+
+function agregarPersona() {
+    const nombre = prompt("Ingrese el nombre de la persona:");
+    const edad = parseInt(prompt("Ingrese la edad de la persona:"));
+
+    if (nombre && !isNaN(edad)) {
+        personas.push({ nombre, edad });
+        actualizarResultados();
+    } else {
+        alert("Ingrese datos válidos.");
+    }
+}
+
+function actualizarResultados() {
+    const personasMayoresDe25 = personas.filter(persona => persona.edad > 25);
+    const nombres = personas.map(persona => persona.nombre);
+    const sumaEdades = personas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
+
+    const personasMayoresDe25List = document.getElementById('personasMayoresDe25');
+    const nombresPersonasList = document.getElementById('nombresPersonas');
+    const sumaEdadesElement = document.getElementById('sumaEdades');
+
+    personasMayoresDe25List.innerHTML = "";
+    nombresPersonasList.innerHTML = "";
+
+    personasMayoresDe25.forEach(persona => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${persona.nombre} (${persona.edad} años)`;
+        personasMayoresDe25List.appendChild(listItem);
+    });
+
+    nombres.forEach(nombre => {
+        const listItem = document.createElement('li');
+        listItem.textContent = nombre;
+        nombresPersonasList.appendChild(listItem);
+    });
+
+    sumaEdadesElement.textContent = `La suma de edades es: ${sumaEdades} años`;
+}
